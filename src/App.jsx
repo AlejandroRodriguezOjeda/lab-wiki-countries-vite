84fb71c8523be2e3032f23ad60c6213733bc1ecf
Alejandro Rodriguez <aperturegir@gmail.com>
2023-08-25 05:14:23 +0100

    almost

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import "bootstrap/dist/css/bootstrap.css"
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { Routes, Route, Link } from 'react-router-dom'
+import HomePage from "./pages/HomePage";
+import CountryDetails from "./pages/CountryDetailsPage";
 
 function App() {
   return (
     <div className="App">
-      <h1>LAB | React WikiCountries</h1>
+      <Navbar />
+
+      <Routes>
+
+<Route path="/" element={ <HomePage /> }/>
+<Route path="/:countryId" element={ <CountryDetails/> }/>
+
+</Routes>
+   
     </div>
   );
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,43 @@
-function HomePage() {}
 
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+import { Link } from "react-router-dom";
+
+
+
+
+function HomePage() {
+   
+      const [allCountries, setAllCountries] = useState(null)
+useEffect(()=>{
+    axios.get("https://ih-countries-api.herokuapp.com/countries")
+     .then((response)=>{
+        console.log(response);
+        setAllCountries(response.data)
+     }).catch((error)=>{
+        console.log(error);
+     })
+                
+        
+},[])
+
+if (allCountries === null) {
+            return <h3>... buscando</h3>
+        }
+return(
+ <>
+    <h1>WikiCountries: Your Guide to the World</h1>
+
+<div>
+        {allCountries.map((eachCountry)=>{
+            return(
+                <li key={eachCountry.alpha3Code}>
+                    <Link to={`/${eachCountry.alpha3Code}`}>{eachCountry.name.official}</Link>         
+                </li>
+            )
+        })}</div>
+
+     </> 
+     )}
 export default HomePage;
